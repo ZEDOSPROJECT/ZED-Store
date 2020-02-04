4f66c66f229cc4d964c505b64c36e03d8180a4fa
Alexandre Nunes <alexandrenunes1331@gmail.com>
2020-02-04 20:58:33 +0000

    [ADD] Add query search

--- getAppList.php
+++ getAppList.php
@@ -3,22 +3,40 @@
 
     $dir          = "./DIRECTORY"; //path
     $list = array(); //main array
+    if(!isset($_GET['query'])){
+        $query="";
+    }
+    $query = $_GET['query'];
 
     if(is_dir($dir)){
         if($dh = opendir($dir)){
             while(($file = readdir($dh)) != false){
-
-                if($file == "." or $file == ".."){
+                if($file == "." or $file == ".." ){
                     //...
                 } else { //create object with two fields
-                    $list3 = array(
-                    'file' => $file, 
-                    'size' => filesize($file));
-                    if($_GET["type"]=="all"){
-                        array_push($list, $list3);
+                    if($query!==""){
+                        if(strstr(strtolower($file), strtolower($query)) == TRUE){
+                            $list3 = array(
+                            'file' => $file, 
+                            'size' => filesize($file));
+                            if($_GET["type"]=="all"){
+                                array_push($list, $list3);
+                            }else{
+                                if(file_get_contents("./DIRECTORY/".$file."/type")==$_GET['type']){
+                                    array_push($list, $list3);
+                                }
+                            }
+                        }
                     }else{
-                        if(file_get_contents("./DIRECTORY/".$file."/type")==$_GET['type']){
+                        $list3 = array(
+                        'file' => $file, 
+                        'size' => filesize($file));
+                        if($_GET["type"]=="all"){
                             array_push($list, $list3);
+                        }else{
+                            if(file_get_contents("./DIRECTORY/".$file."/type")==$_GET['type']){
+                                array_push($list, $list3);
+                            }
                         }
                     }
                 }
